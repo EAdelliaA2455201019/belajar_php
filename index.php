89624fa9cfd0a2c854e0f445af2d9a4fcb6ec91a
adellia <adelagustin2006@gmail.com>
2026-06-08 23:24:42 +0700

    feat: implementasi form tambah produk dan konfigurasi database

--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
+
+// memanggil file konfigurasi database
 include 'config.php';
 
 try {
+
     $pdo = new PDO($dsn, $user, $pass);
+
     $pdo->setAttribute(
         PDO::ATTR_ERRMODE,
         PDO::ERRMODE_EXCEPTION
     );
+
+    // menampilkan pesan jika koneksi berhasil
     echo "Koneksi Berhasil <br>";
 
     $query = "SELECT * FROM products";
+
     $stmt = $pdo->query($query);
+
     $products = $stmt->fetchAll();
-  
+
+    // menampilkan data produk
     foreach ($products as $product) {
-        echo $product['name'] . "<br>";
+
+        echo htmlspecialchars($product['name']) .
+        " - Rp " .
+        number_format($product['price'], 0, ",", ".") .
+        "<br>";
+
     }
 
 } catch (PDOException $e) {
+
+    // menampilkan pesan jika koneksi gagal
     die(" Koneksi Gagal : " . $e->getMessage());
+
 }
+
+?>
